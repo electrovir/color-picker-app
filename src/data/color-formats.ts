0ea8d08d6bc3eb9d350ac367022c3c28136e8a61
electrovir <electrovir@users.noreply.github.com>
2025-06-05 14:12:38 +0000

    explicitly define all color formats

--- src/data/color-formats.ts
+++ src/data/color-formats.ts
@@ -1,10 +1,20 @@
 import {check} from '@augment-vir/assert';
-import {filterMap, getObjectTypedEntries, mapObjectValues, round} from '@augment-vir/common';
+import {
+    type AnyObject,
+    filterMap,
+    getObjectTypedEntries,
+    mapObjectValues,
+    type PartialWithUndefined,
+    round,
+    typedObjectFromEntries,
+    type UnionToIntersection,
+    type Values,
+} from '@augment-vir/common';
 import colorNames from 'color-name';
 import type Color from 'colorjs.io';
 import {colorNameLength} from './color-name-length.js';
 
-function wrapNaN(value: number) {
+function wrapNaN(value: number): number | string {
     if (isNaN(value)) {
         return 'none';
     } else {
@@ -12,160 +22,213 @@ function wrapNaN(value: number) {
     }
 }
 
-export type ColorCoords = Record<
-    string,
-    {
-        min: number;
-        max: number;
-        canBeNone?: boolean | undefined;
-    }
->;
+export type ColorFormatDefinition = {
+    coords: Record<
+        string,
+        {
+            min: number;
+            max: number;
+        } & PartialWithUndefined<{
+            canBeNone?: boolean | undefined;
+            /**
+             * The number of digits to round this coordinate to.
+             *
+             * @default 0
+             */
+            digits?: number | undefined;
+            /**
+             * A factor to multiple the values in the `Color` class by.
+             *
+             * @default 1
+             */
+            factor?: number | undefined;
+        }>
+    >;
+    /** If the format's name isn't exactly the input for `Color.to()`, then specify it here. */
+    convertTo?: string | undefined;
+};
 
-export const colorFormats = {
+type BaseColorFormatDefinitions = {
+    [ColorSpaceName in string]: {[CoordFormatName in string]: ColorFormatDefinition};
+};
+
+export const colorFormats = (<const Definitions extends BaseColorFormatDefinitions>(
+    input: Definitions,
+): Readonly<{
+    [ColorSpaceName in keyof Definitions]: {
+        [ColorFormatName in keyof Definitions[ColorSpaceName]]: ColorFormatDefinition;
+    };
+}> => {
+    return input;
+})({
     srgb: {
         RGB: {
-            r: {
-                min: 0,
-                max: 255,
+            coords: {
+                r: {
+                    min: 0,
+                    max: 255,
+                    factor: 255,
+                },
+                g: {
+                    min: 0,
+                    max: 255,
+                    factor: 255,
+                },
+                b: {
+                    min: 0,
+                    max: 255,
+                    factor: 255,
+                },
             },
-            g: {
-                min: 0,
-                max: 255,
-            },
-            b: {
-                min: 0,
-                max: 255,
-            },
+            convertTo: 'srgb',
         },
         HSL: {
-            h: {
-                min: 0,
-                max: 360,
-                canBeNone: true,
-            },
-            s: {
-                min: 0,
-                max: 100,
-            },
-            l: {
-                min: 0,
-                max: 100,
+            coords: {
+                h: {
+                    min: 0,
+                    max: 360,
+                    canBeNone: true,
+                },
+                s: {
+                    min: 0,
+                    max: 100,
+                },
+                l: {
+                    min: 0,
+                    max: 100,
+                },
             },
         },
         HSV: {
-            h: {
-                min: 0,
-                max: 360,
-                canBeNone: true,
-            },
-            s: {
-                min: 0,
-                max: 100,
-            },
-            v: {
-                min: 0,
-                max: 100,
+            coords: {
+                h: {
+                    min: 0,
+                    max: 360,
+                    canBeNone: true,
+                },
+                s: {
+                    min: 0,
+                    max: 100,
+                },
+                v: {
+                    min: 0,
+                    max: 100,
+                },
             },
         },
         HWB: {
-            h: {
-                min: 0,
-                max: 360,
-                canBeNone: true,
-            },
-            s: {
-                min: 0,
-                max: 100,
-            },
-            v: {
-                min: 0,
-                max: 100,
+            coords: {
+                h: {
+                    min: 0,
+                    max: 360,
+                    canBeNone: true,
+                },
+                w: {
+                    min: 0,
+                    max: 100,
+                },
+                b: {
+                    min: 0,
+                    max: 100,
+                },
             },
         },
     },
     CIELAB: {
         LAB: {
-            l: {
-                min: 0,
-                max: 100,
-                canBeNone: true,
-            },
-            a: {
-                min: -128,
-                max: 127,
-                canBeNone: true,
-            },
-            b: {
-                min: -128,
-                max: 127,
-                canBeNone: true,
+            coords: {
+                l: {
+                    min: 0,
+                    max: 100,
+                    canBeNone: true,
+                },
+                a: {
+                    min: -128,
+                    max: 127,
+                    canBeNone: true,
+                },
+                b: {
+                    min: -128,
+                    max: 127,
+                    canBeNone: true,
+                },
             },
         },
         LCH: {
-            l: {
-                min: 0,
-                max: 100,
-                canBeNone: true,
-            },
-            c: {
-                min: 0,
-                max: 230,
-                canBeNone: true,
-            },
-            h: {
-                min: 0,
-                max: 360,
-                canBeNone: true,
+            coords: {
+                l: {
+                    min: 0,
+                    max: 100,
+                    canBeNone: true,
+                },
+                c: {
+                    min: 0,
+                    max: 230,
+                    canBeNone: true,
+                },
+                h: {
+                    min: 0,
+                    max: 360,
+                    canBeNone: true,
+                },
             },
         },
     },
     Oklab: {
         Oklab: {
-            l: {
-                min: 0,
-                max: 1,
-                canBeNone: true,
-            },
-            a: {
-                min: -0.5,
-                max: 0.5,
-                canBeNone: true,
-            },
-            b: {
-                min: -0.5,
-                max: 0.5,
-                canBeNone: true,
+            coords: {
+                l: {
+                    min: 0,
+                    max: 1,
+                    canBeNone: true,
+                    digits: 2,
+                },
+                a: {
+                    min: -0.5,
+                    max: 0.5,
+                    canBeNone: true,
+                    digits: 2,
+                },
+                b: {
+                    min: -0.5,
+                    max: 0.5,
+                    canBeNone: true,
+                    digits: 2,
+                },
             },
         },
         Oklch: {
-            l: {
-                min: 0,
-                max: 1,
-                canBeNone: true,
-            },
-            c: {
-                min: 0,
-                max: 0.4,
-                canBeNone: true,
-            },
-            h: {
-                min: 0,
-                max: 360,
-                canBeNone: true,
+            coords: {
+                l: {
+                    min: 0,
+                    max: 1,
+                    canBeNone: true,
+                    digits: 2,
+                },
+                c: {
+                    min: 0,
+                    max: 0.4,
+                    canBeNone: true,
+                    digits: 2,
+                },
+                h: {
+                    min: 0,
+                    max: 360,
+                    canBeNone: true,
+                    digits: 2,
+                },
             },
         },
     },
-} satisfies {[ColorSpaceName in string]: {[CoordFormatName in string]: ColorCoords}};
+});
+
+export type ColorFormatName = keyof UnionToIntersection<Values<typeof colorFormats>>;
+export type ColorCoordinate = keyof UnionToIntersection<
+    Values<UnionToIntersection<Values<typeof colorFormats>>>
+>;
 
 export function createColorStrings(color: Color) {
     const rgb = color.to('srgb');
-    const hsl = color.to('hsl');
-    const hwb = color.to('hwb');
-    const lab = color.to('lab');
-    const lch = color.to('lch');
-    const hsv = color.to('hsv');
-    const oklab = color.to('oklab');
-    const oklch = color.to('oklch');
 
     const colorNames = findMatchingColorNames([
         Math.round(rgb.r * 255),
@@ -173,60 +236,49 @@ export function createColorStrings(color: Color) {
         Math.round(rgb.b * 255),
     ]);
 
+    const formattedColorFormats = typedObjectFromEntries(
+        Object.values(colorFormats).flatMap((colorSpace) => {
+            return Object.entries(colorSpace).map(
+                ([
+                    colorFormatName,
+                    colorFormat,
+                ]) => {
+                    const converted = color.to(colorFormat.convertTo || colorFormatName);
+
+                    const coordValues = getObjectTypedEntries(colorFormat.coords).map(
+                        ([
+                            coordKey,
+                            coord,
+                        ]) => {
+                            const coordValue = round(
+                                (converted as AnyObject)[coordKey] * (coord.factor || 1),
+                                {digits: coord.digits || 0},
+                            );
+
+                            if (coord.canBeNone) {
+                                return wrapNaN(coordValue);
+                            } else {
+                                return coordValue;
+                            }
+                        },
+                    );
+
+                    return [
+                        colorFormatName as ColorFormatName,
+                        coordValues,
+                    ];
+                },
+            );
+        }),
+    );
+
     const colorCoords = {
-        Hex: [String(rgb.display({format: 'hex', collapse: false}))],
-        RGB: [
-            Math.round(rgb.r * 255),
-            Math.round(rgb.g * 255),
-            Math.round(rgb.b * 255),
+        Hex: [
+            String(rgb.display({format: 'hex', collapse: false})),
         ],
-        HSL: [
-            /** Pad to 4 because `h` may be `'none'`. */
-            wrapNaN(Math.round(hsl.h)),
-            Math.round(hsl.s),
-            Math.round(hsl.l),
-        ],
-        HSV: [
-            /** Pad to 4 because `h` may be `'none'`. */
-            wrapNaN(Math.round(hsv.h)),
-            Math.round(hsv.s),
-            Math.round(hsv.v),
-        ],
-        HWB: [
-            wrapNaN(Math.round(hwb.h)),
-            // 0 - 100
-            Math.round(hwb.w),
-            // 0 - 100
-            Math.round(hwb.b),
-        ],
-        LAB: [
-            // 0 - 100
-            Math.round(lab.l),
-            // -125 - 125
-            Math.round(lab.a),
-            // -125 - 125
-            Math.round(lab.b),
-        ],
-        LCH: [
-            // 0 - 100
-            Math.round(lch.l),
-            // 0 - ~230
-            Math.round(lch.c),
-            // 0 - 360
-            wrapNaN(Math.round(lch.h)),
-        ],
-        Oklab: [
-            wrapNaN(round(oklab.l, {digits: 2})),
-            wrapNaN(round(oklab.a, {digits: 2})),
-            wrapNaN(round(oklab.b, {digits: 2})),
-        ],
-        Oklch: [
-            wrapNaN(round(oklch.l, {digits: 2})),
-            wrapNaN(round(oklch.c, {digits: 2})),
-            wrapNaN(round(oklch.h, {digits: 1})),
-        ],
+        ...formattedColorFormats,
         Name: [colorNames.join(', ').padEnd(colorNameLength, ' ')],
-    } satisfies Record<string, (string | number)[]>;
+    } satisfies Record<ColorFormatName | 'Hex' | 'Name', (string | number)[]>;
 
     return mapObjectValues(colorCoords, (key, values) => {
         const paddedValues =
